25c7b2d173275699783636e6fee99219aa458415
Yaroslav <122485322+Yaroslav1403@users.noreply.github.com>
2024-12-18 10:30:27 +0300

    Task 2 complete

--- collision /IsCorrectRect.py
+++ collision /IsCorrectRect.py
@@ -1,11 +1,10 @@
 #Функции isCorrectRect принимает аргумент rectangles
-def isCorrectRect(rectangles):
+def isCorrectRec(rectangles):
     #Цикл for перебирает все элементы в rectangles и функция enumerate возвращает элемент rect и индекс элемента index
     for index, rect in enumerate(rectangles, start = 1):
         #Проверяем корректность прямоугольника
         if rect[0][0] >= rect[1][0] or rect[0][1] >= rect[1][1]:
             #Если прямоугольник некорректен, то вызывается исключение ValueError с сообщением номерf некорректного прямоугольника
             raise ValueError(f'Некорректный прямоугольник с номером {index}')
-    
     #Если все прямоугольники корректны, функция возвращает True
     return True
